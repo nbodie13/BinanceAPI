956c535fe7c8708b7bff67bd0ad897ffcf1336ee
nbodie13 <bodenschatz.nathan@gmail.com>
2021-05-17 14:24:37 -0400

    Update 5/17/21

--- ta_backtest.py
+++ ta_backtest.py
@@ -1,5 +1,5 @@
 import numpy, talib, os, math
-from numpy import genfromtxt, NaN
+from numpy import asmatrix, genfromtxt, NaN
 
 #import data and get closing price; comment out if pulling functions from this file.
 dataset = 'data/15minutes.csv'
@@ -63,12 +63,11 @@ def ema(dataset, period = 10, scale_factor=1):
     if period > len(dataset):
         return("Period too large. Reduce period or gather more data.")
 
-    # Calculate the simple moving average of dataset
+    # Calculate the exponential moving average of dataset
     i = 0
     ema = []
     this_window = []
     multiplier = 2/(period+1)
-    print(len(dataset-1))
     while i < period-1:
         ema.append(math.nan)
         this_window.append(dataset[i])
@@ -77,13 +76,41 @@ def ema(dataset, period = 10, scale_factor=1):
         this_window.append(dataset[i])
         ema.append(sum(this_window)/period)
         i += 1
-    while i >= period & i < len(dataset):
-        # ema.append((dataset[i]-ema[-1])*multiplier+ema[-1])
-        print(i)
+    while i >= period and i < len(dataset):
+        ema.append((dataset[i]-ema[-1])*multiplier+ema[-1])
         i += 1
     return ema
 
-# Get results of EMA function
-ema_ = ema(close,10)
-# print(ema_)
-# print(len(ema_))
+    # Get results of EMA function
+    # ema_ = ema(close,10)
+    # print(ema_)
+    # print(len(ema_))
+
+####### Moving Average Convergence Divergence #######
+def macd(dataset, period = 9, fastlength = 12, slowlength = 26, scale_factor = 1):
+
+    # Scale the dataset if you don't want to use the give data timeframe
+    dataset = scale_data(dataset,scale_factor)
+
+    # Error if period is too large
+    if period > len(dataset):
+        return("Period too large. Reduce period or gather more data.")
+    
+    i = 0
+    dif = []
+    ema_d = []
+    ema_fast = ema(dataset,fastlength)
+    ema_slow = ema(dataset,slowlength)
+    while i < len(dataset):
+        if i >= slowlength:
+            dif.append(ema_fast[i] - ema_slow[i])
+        i += 1
+    ema_d = ema(dif,period)
+    print(ema_d)
+    return dif, ema_d
+
+    # Get results of EMA function
+    # macd_ = macd(close)
+    # print(macd_[1])
+    # print(len(ema_))
+
