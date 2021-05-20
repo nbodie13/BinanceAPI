1886536808f9d3d4c411a451fcb4efd55aadc390
nbodie13 <bodenschatz.nathan@gmail.com>
2021-05-20 09:40:00 -0400

    Update 5/19/21

--- trading-bot.py
+++ trading-bot.py
@@ -1,35 +1,47 @@
-import websocket, json, pprint
+from time import time
+from binance.enums import KLINE_INTERVAL_1MINUTE
+import websocket, json, pprint, talib, numpy
+import config, csv
+from binance.client import Client
+
+client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
 pair = 'btcusdt'
-timeframe = '1m'
-SOCKET = 'wss://stream.binance.com:9443/ws/%s@kline_%s' % (pair, timeframe)
+timeframe = 1
+SOCKET = 'wss://stream.binance.com:9443/ws/%s@kline_%dm' % (pair, timeframe)
+kline_int = 'Client.KLINE_INTERVAL_%dMINUTE' % (timeframe)
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
 
+# minutes = str(timeframe*RSI_PERIOD)
+# print(minutes)
+candles = client.get_historical_klines("BTCUSDT",Client.KLINE_INTERVAL_1MINUTE,"14 minutes ago")
+print(candles)
 closes = []
-i = 0
 
-def on_open(ws):
-    print('opened connection')
+# def on_open(ws):
+#     print('opened connection')
 
-def on_close(ws):
-    print('closed connection')
+# def on_close(ws):
+#     print('closed connection')
 
-def on_message(ws, message):
-    # print('received message')
-    json_message = json.loads(message)
-    # pprint.pprint(json_message)
+# def on_message(ws, message):
+#     # print('received message')
+#     json_message = json.loads(message)
+#     # pprint.pprint(json_message)
 
-    candle = json_message['k']
-    is_candle_closed = candle['x']
-    close = candle['c']
+#     candle = json_message['k']
+#     is_candle_closed = candle['x']
+#     close = candle['c']
 
-    if is_candle_closed:
-        print("candle closed at {}".format(close))
-        closes.append(float(close))
-        print("closes")
-        print(closes)
-        i += 1
-        if i > 20:
-            closes.pop(0)
+#     if is_candle_closed:
+#         # print("candle closed at {}".format(close))
+#         closes.append(float(close))
+#         print(closes)
 
-ws = websocket.WebSocketApp(SOCKET, on_open = on_open, on_close = on_close, on_message = on_message)
-ws.run_forever()
+#         if len(closes) >= 20:
+#             closes.pop(0)
+
+# ws = websocket.WebSocketApp(SOCKET, on_open = on_open, on_close = on_close, on_message = on_message)
+# ws.run_forever()
